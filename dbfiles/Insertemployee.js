8bb20c126299bb41fdf312afb7441ce98a6ed926
SanaGithubUber <sanagithububer@gmail.com>
2025-01-28 18:22:34 +0400

    React_menus

---
+++ dbfiles/Insertemployee.js
@@ -0,0 +1,20 @@
+class Employees{
+constructor ( Employee_Code 
+    , Employee_Name 
+    , Date_of_birth 
+    , Date_of_joining 
+    , Department 
+    , Report_to 
+    , Contact_no 
+    , Resigned_status 
+    , Resigned_date ){this.Employee_Code=Employee_Code ;
+        this.Employee_Name =Employee_Name;
+        this.Date_of_birth =Date_of_birth
+        this.Date_of_joining =Date_of_joining
+        this.Department =Department
+        this.Report_to =Report_to
+        this.Contact_no =Contact_no
+        this.Resigned_status= Resigned_status
+        this.Resigned_date=Resigned_date }     
+}
+module.exports=Employees
